4ba7fef914cfbce60e62f876ccafc74b0606591d
Solder@Code <alexey.morozov.2023@mail.ru>
2025-10-18 22:19:05 +0300

    Update EasyENC.h

--- src/EasyENC.h
+++ src/EasyENC.h
@@ -1,16 +1,17 @@
 class EasyENC {
 public:
-  EasyENC(uint8_t SW, uint8_t DT, uint8_t CLK)
-    : _SW(SW), _DT(DT), _CLK(CLK) {}
+  EasyENC(uint8_t DT, uint8_t CLK)
+    : _DT(DT), _CLK(CLK) {}
   // переменные
   int8_t direct;
   // инициализация(обязательно)
-  void init() {
+  void init(uint8_t typeEnc) {
     //  обьявляем пины как вход
-    pinMode(_SW, INPUT_PULLUP);
     pinMode(_DT, INPUT_PULLUP);
     pinMode(_CLK, INPUT_PULLUP);
     _lastState = digitalRead(_CLK);
+    //переменную типа из публичной в приватную
+    _typeEnc = typeEnc;
   }
   // тикер, меняет значение переменой(вызывать в луп)
   void tick() {
@@ -20,28 +21,18 @@ public:
     } else direct = 0;
     _lastState = _state;
   }
-
-  // тикр кнопки, вовзращает True при нажатой кнопке. В функцию указать уровень кнопки при нажатии
-  boolean tickSW(boolean lvlBtn) {
-    if (millis() - _tmrSwRd >= 30) {
-      if (digitalRead(_SW) == lvlBtn) return true;
-      else return false;
-      _tmrSwRd = millis();
-    }
-  }
-
+  // приватная зона
 private:
-  uint8_t _SW, _DT, _CLK, _counter1, _counter2;
+  // переменные
+  uint8_t _DT, _CLK, _counter1, _counter2, _typeEnc;
   boolean _state, _lastState;
-  uint32_t _tmrSwRd, _tmrZeroDrct;
-  boolean numBtn;
-
-  void direct1(){  
-    if(++_counter1 > 2)_counter1 = 1;
-    if(_counter1 == 2)direct = 1;
+  //обработчики 2х и более срабатываний
+  void direct1() {
+    if (++_counter1 > _typeEnc) _counter1 = 1;
+    if (_counter1 == 2) direct = 1;
   }
-  void direct2(){
-    if(++_counter2 > 2)_counter2 = 1; 
-    if(_counter2 == 2)direct = -1;
+  void direct2() {
+    if (++_counter2 > _typeEnc) _counter2 = 1;
+    if (_counter2 == 2) direct = -1;
   }
 };
