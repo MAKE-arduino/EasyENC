319dae1ee5ca23d655433f1da890e34c9b794406
Solder@Code <alexey.morozov.2023@mail.ru>
2025-10-18 19:14:05 +0300

    Update EasyENC.h

--- src/EasyENC.h
+++ src/EasyENC.h
@@ -1,6 +1,6 @@
-class SCenc {
+class EasyENC {
 public:
-  SCenc(uint8_t SW, uint8_t DT, uint8_t CLK)
+  EasyENC(uint8_t SW, uint8_t DT, uint8_t CLK)
     : _SW(SW), _DT(DT), _CLK(CLK) {}
   // переменные
   int8_t direct;
@@ -16,25 +16,32 @@ public:
   void tick() {
     _state = digitalRead(_CLK);
     if (_state != _lastState) {
-      digitalRead(_DT) != _state ? direct = 1 : direct = -1;
+      digitalRead(_DT) != _state ? direct1() : direct2();
+    } else direct = 0;
+    _lastState = _state;
+  }
+
+  // тикр кнопки, вовзращает True при нажатой кнопке. В функцию указать уровень кнопки при нажатии
+  boolean tickSW(boolean lvlBtn) {
+    if (millis() - _tmrSwRd >= 30) {
+      if (digitalRead(_SW) == lvlBtn) return true;
+      else return false;
+      _tmrSwRd = millis();
     }
   }
-  else direct = 0;
-  _lastState = _state;
-}
-
-// тикр кнопки, вовзращает True при нажатой кнопке. В функцию указать уровень кнопки при нажатии
-boolean tickSW(boolean lvlBtn) {
-  if (millis() - _tmrSwRd >= 30) {
-    if (digitalRead(_SW) == lvlBtn) return true;
-    else return false;
-    _tmrSwRd = millis();
-  }
-}
 
 private:
-uint8_t _SW, _DT, _CLK;
-boolean _state, _lastState;
-uint32_t _tmrSwRd, _tmrZeroDrct;
-boolean numBtn;
+  uint8_t _SW, _DT, _CLK, _counter1, _counter2;
+  boolean _state, _lastState;
+  uint32_t _tmrSwRd, _tmrZeroDrct;
+  boolean numBtn;
+
+  void direct1(){  
+    if(++_counter1 > 2)_counter1 = 1;
+    if(_counter1 == 2)direct = 1;
+  }
+  void direct2(){
+    if(++_counter2 > 2)_counter2 = 1; 
+    if(_counter2 == 2)direct = -1;
+  }
 };
