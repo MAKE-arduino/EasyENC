162b48c667bead3f0b4ad2852f6e900239082ff0
Solder@Code <alexey.morozov.2023@mail.ru>
2025-10-18 08:42:09 +0300

    Update EasyENC.h

--- src/EasyENC.h
+++ src/EasyENC.h
@@ -6,32 +6,19 @@ class EasyENC {
     EasyENC(uint8_t SW, uint8_t DT, uint8_t CLK)
       : _SW(SW), _DT(DT), _CLK(CLK) {}
     
-    void init(uint8_t typeEnc) {
+    void init() {
       pinMode(_SW, INPUT_PULLUP);
       pinMode(_DT, INPUT_PULLUP);
       pinMode(_CLK, INPUT_PULLUP);
-      _typeEnc = typeEnc;
+      currentState = digitalRead(CLK);  
     }
     int tick() {
-      _pA = digitalRead(_CLK);
-      _pB = digitalRead(_DT);
-      if (_lastState != pA) {
-        switch (_typeEnc) {
-          case 1: // EB_STEP4_LOW
-            if (!(e0 & e1)) return 0;  // skip 01, 10, 00
-            break;
-          case 2: // EB_STEP4_HIGH
-            if (e0 | e1) return 0;  // skip 01, 10, 11
-            break;
-          case 3: // EB_STEP2
-            if (e0 ^ e1) return 0;  // skip 10 01
-            break;
-        }
-        _lastState = _pA;
-        return pB ^ pA
-      }
-      return 0;
-    }
+  _currentState = digitalRead(CLK);
+   if (_currentState != _initState  && _currentState == 1) {
+    digitalRead(DT) != currentState ? return 1 : return -1;
+   }
+  _initState = _currentState;
+}
     boolean tickSW(boolean lvlBtn) {
       if (millis() - _tmrSwRd >= 30) {
         if (digitalRead(_SW) == lvlBtn) return true;
@@ -41,6 +28,6 @@ class EasyENC {
     }
   private:
     uint8_t _SW, _DT, _CLK, _pA, _pB, _typeEnc;
-    boolean _lastState;
+    boolean _initState, _currentState;
     uint32_t _tmrSwRd;
 };
