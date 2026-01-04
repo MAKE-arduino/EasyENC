78c8a1079d06626074b4438df1ea259bd6a4b906
Solder&Code <alexey.morozov.2023@mail.ru>
2026-01-04 20:23:42 +0300

    Update EasyENC.h

--- src/EasyENC.h
+++ src/EasyENC.h
@@ -1,60 +1,50 @@
 #pragma once
 #include <Arduino.h>
 
-class EasyENC {
-  public:
-    EasyENC(uint8_t DT, uint8_t CLK)
-      : _DT(DT), _CLK(CLK) {}
+template<uint8_t DT, uint8_t CLK, uint8_t typeEnc>
+struct EasyENC {
+  EasyENC() {
+    //  обьявляем пины как вход
+    pinMode(DT, INPUT_PULLUP);
+    pinMode(CLK, INPUT_PULLUP);
+    e0 = digitalRead(CLK);
+    e1 = digitalRead(DT);
+  }
+  
+  //обработка энкодера
+  int8_t epos;
+  boolean p0, p1, e0, e1;
+  int8_t direct;
 
-    // переменные
-    int8_t direct;
+  // обработчик энкодера
+  int8_t pollEnc() {
+    e0 = digitalRead(CLK);
+    e1 = digitalRead(DT);
+    if (p0 ^ p1 ^ e0 ^ e1) {
+      (p1 ^ e0) ? ++epos : --epos;
+      p0 = e0, p1 = e1;
+      if (!epos) direct = 0;
 
-    // инициализация(обязательно)
-    void init(uint8_t typeEnc) {
-      //  обьявляем пины как вход
-      pinMode(_DT, INPUT_PULLUP);
-      pinMode(_CLK, INPUT_PULLUP);
-      _e0 = digitalRead(_CLK);
-      _e1 = digitalRead(_DT);
-      //переменную типа из публичной в приватную
-      _typeEnc = typeEnc;
-    }
-
-    // тикер, меняет значение переменой(вызывать в луп)
-    void tick() {
-      direct = pollEnc();
-    }
-    // приватная зона
-
-  private:
-    // переменные
-    int8_t _epos;
-    uint8_t _DT, _CLK, _typeEnc;
-    boolean _p0, _p1, _e0, _e1;
-    // обработчик энкодера
-    int8_t pollEnc() {
-      _e0 = digitalRead(_CLK);
-      _e1 = digitalRead(_DT);
-      if (_p0 ^ _p1 ^ _e0 ^ _e1) {
-        (_p1 ^ _e0) ? ++_epos : --_epos;
-        _p0 = _e0, _p1 = _e1;
-        if (!_epos) direct = 0;
-
-        switch (_typeEnc) {
-          case 1:
-            if (!(_e0 & _e1)) return 0;  // skip 01, 10, 00
-            break;
-          case 2:
-            if (_e0 | _e1) return 0;  // skip 01, 10, 11
-            break;
-          case 3:
-            if (_e0 ^ _e1) return 0;  // skip 10 01
-            break;
-        }
-        int8_t state = _epos > 0 ? -1 : 1;
-        _epos = 0;
-        return state;
+      switch (typeEnc) {
+        case 1:
+          if (!(e0 & e1)) return 0;  // скип 01, 10, 00
+          break;
+        case 2:
+          if (e0 | e1) return 0;  // скип 01, 10, 11
+          break;
+        case 3:
+          if (e0 ^ e1) return 0;  // скип 10 01
+          break;
       }
-      return 0;
+      int8_t state = epos > 0 ? -1 : 1;
+      epos = 0;
+      return state;
     }
+    return 0;
+  }
+
+  // тикер, меняет значение переменой(вызывать в луп)
+  void tick() {
+    direct = pollEnc();
+  }
 };
